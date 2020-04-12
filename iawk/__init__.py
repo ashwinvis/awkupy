1e800de5ba3904c65a1078350afd55fa09e37e24
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-04-13 00:38:41 +0200

    Allow awk to work as line magic and return output

--- iawk/__init__.py
+++ iawk/__init__.py
@@ -6,18 +6,34 @@ from awkupy import Awk
 
 @magics_class
 class AwkMagics(Magics):
+    "Provides the %awk line magic and %%awk cell magic."""
 
     @line_magic
     @cell_magic
     @magic_arguments()
+    @argument("code", nargs="?", default="")
     @argument("input")
-    def awk(self, line, cell):
+    def awk(self, line, cell=""):
+        """Execute awk via awkupy API.
+
+        Examples::
+
+            %load_ext iawk
+
+            %%awk LICENSE
+            /^  [0-9]/{print $0}
+
+            output = %awk '/^  [0-9]/{print $0}' LICENSE
+            output.splitlines()[0]
+
+        """
         args = parse_argstring(self.awk, line)
 
         api = Awk()
         api.INPUT = args.input
-        api.code = cell
-        api()
+        args.code = args.code.strip("'")
+        api.code = "".join((args.code, cell))
+        return api.check_output().decode()
 
 
 def load_ipython_extension(ipython):
