7720c83eb166fe2a9c40d8dd9b21b68133b4a3f2
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-04-13 15:22:50 +0200

    src layout and update version

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-import os
-from runpy import run_path
 
-d = run_path(os.path.join('awkupy', '_version.py'))
-__version__ = d['__version__']
-
-setup(
-    name='awkupy',
-    url='https://github.com/jadelord/awkupy',
-    download_url='https://github.com/jadelord/awkupy',
-    version=__version__,
-    description='A simple Python interface for running AWK natively',
-    license='GPL',
-    platforms=['any'],
-    author='Ashwin Vishnu',
-    author_email='avmo@kth.se',
-    packages=find_packages(exclude=['doc', 'tests']),
-    entry_points={
-        "console_scripts": "iawk=iawk.__main__:main",
-    },
-    install_requires=["ipython"],
-)
+setup()
